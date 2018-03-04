679601d379c5c2a50ec631be86517ff01836626e
Ironman2121 <36769222+Ironman2121@users.noreply.github.com>
2018-03-04 12:01:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
-const Discord = require('discord.js-commando');
-const client = new Discord.Client();
+16 lines (11 sloc)  419 Bytes
+const commando = require('discord.js-commando')
+const bot = new commando.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+bot.on('message', (message) => {
+    if(message.content == 'hi'){
+        message.reply('yo');
+    }
+})
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.registry.registerGroup('random', 'random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+
+bot.login(process.env.BOT_TOKEN);
